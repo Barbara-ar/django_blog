755fba1bfc427f2a13ef96e1575fc11ef86fb351
Barbara <cabralba98@gmail.com>
2024-11-21 19:50:49 -0300

    Actualización: ajustes en blog/views.py y x/urls.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
 
 def HelloWord(request):
-    return HttpResponse('<H1>Hello Word<H1>')
+    return HttpResponse('<h1>Hello World</h1>')  # Etiquetas correctamente cerradas
